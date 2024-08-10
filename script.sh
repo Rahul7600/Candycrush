de632c626a4f908afd304064605236b1ce28f77b
rahul <devdev@gmail.com>
2024-08-10 15:30:45 +0530

    reverted
    Revert "Update script.sh"
    
    This reverts commit f8f1a9139c1a9a86396312dc5d957739606adeb1.

--- script.sh
+++ script.sh
@@ -22,7 +22,7 @@ sudo apt update && sudo apt install terraform
 
 # Install kubectl
 sudo apt update
-sudo apt install curl -yy
+sudo apt install curl -y
 curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
