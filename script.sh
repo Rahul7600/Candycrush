bde5b2684235047eab70fe2bf8fa8d5d55e9e574
rahul <devdev@gmail.com>
2024-08-10 15:33:36 +0530

    yes Revert "Update script.sh"
    This reverts commit aed7471ba2e5b21142cd8b729636f8e016e2c5c6.

--- script.sh
+++ script.sh
@@ -37,4 +37,4 @@ sudo ./aws/install
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x focal main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
-sudo apt install -yy nodejs
+sudo apt install -y nodejs
